aad825b567ed4527c0b456ecba9353630d0ac4e5
omar Collazos Reyes <obcollazos@gmail.com>
2017-09-13 14:25:15 -0400

    segundo commit configuracion grafica

--- src/proyectodao/ProyectoDAO.java
+++ src/proyectodao/ProyectoDAO.java
@@ -16,6 +16,8 @@ public class ProyectoDAO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola a todos");
+        
     }
     
 }
